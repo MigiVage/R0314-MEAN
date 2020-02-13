8d5d5da39079d9a2471abefd7dc8da53794244c1
MigiVage <60513358+MigiVage@users.noreply.github.com>
2020-02-13 11:03:36 +0200

    Add files via upload

---
+++ WS2/workshop2.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+
+console.log("program started");
+fs.readFile('example.txt', results);
+
+for (var i=0; i <15; i++){
+    console.log("Node just keeps on going while the file is being read...");
+}
+function results(err, data){
+    if (err) return console.error(err);
+    console.log("Results of fileread:");
+    console.log(data.toString());
+}
